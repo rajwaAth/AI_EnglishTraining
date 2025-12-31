d96aa57b67173fe0fe424ebcfd2db6cf67d8f3d6
Your Name <you@example.com>
2025-12-31 11:17:21 +0700

    update

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./chat.db"
+DATABASE_URL = "sqlite:///storage/chat.db"
 
 engine = create_engine(
     DATABASE_URL,
